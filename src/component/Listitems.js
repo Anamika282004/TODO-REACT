e6a7114161de04195a773c3cb923ed28355a663f
Anamika <anamikaparoo@gmail.com>
2024-04-26 00:54:52 +0530

    commit

--- src/component/Listitems.js
+++ src/component/Listitems.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { useState } from 'react';
 function Listitems({itemvalue,setitemvalue}) { 
     const[val,setval] = useState(false);
-    const store = itemvalue.map((e,index)=>{return  <div class="flex flex-row justify-center list-none">
+  return (
+    <div>
+        {itemvalue.map((e,index)=>{return  <div class="flex flex-row justify-center list-none">
     <li key={index} contentEditable={val.toString()}>{e}<button onClick={()=>{setval(true)}}>&#128393;</button>  
     <button onClick={(index)=>{
         const newTasks = [...itemvalue];
         newTasks.splice(index,1);
         setitemvalue(newTasks);
     }}>&#10060; </button></li>
-    </div>})
-  return (
-    <div>
-        {store}
+    </div>})}
     </div> 
   )
 }
